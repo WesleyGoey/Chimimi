e004d31c12e5546b43a923a44674593c8b7639c3
WesleyGoey <wesley.goey@gmail.com>
2025-10-12 22:05:16 +0700

    Update mainlayout.blade.php

--- Chimimi/resources/views/layout/mainlayout.blade.php
+++ Chimimi/resources/views/layout/mainlayout.blade.php
@@ -17,7 +17,7 @@
         @yield('content')
     </div>
     @include('layout.footer')
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
